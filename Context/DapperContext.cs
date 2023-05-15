41a30c441f6e408c3a5120dfc8c7a4e5bca815ef
James Dayo <jdayo2012@gmail.com>
2023-05-15 17:51:28 +0100

    fixed context error

--- Context/DapperContext.cs
+++ Context/DapperContext.cs
@@ -9,10 +9,10 @@ namespace DapperCrud.Context
 		private readonly IConfiguration _configuration;
         private readonly string _connectionString;
 
-        public DapperContext(IConfiguration configuration, string connectionString)
+        public DapperContext(IConfiguration configuration)
         {
             _configuration = configuration;
-            _connectionString = configuration.GetConnectionString("sqlConnection");
+            _connectionString = _configuration.GetConnectionString("sqlConnection");
         }
 
         public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
